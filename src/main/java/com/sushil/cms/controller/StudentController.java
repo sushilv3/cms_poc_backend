7da1c4c46fe78331e525a30e8654facfa66a9702
sushil <sushil.verma@walkingtree.tech>
2023-05-23 19:05:41 +0530

    added save Student student api

--- src/main/java/com/sushil/cms/controller/StudentController.java
+++ src/main/java/com/sushil/cms/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.sushil.cms.controller;
 
+import com.sushil.cms.entity.Student;
+import com.sushil.cms.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/")
+    public Student saveStudent(@RequestBody Student student) {
+        System.out.println("student "+ student);
+        return studentService.saveStudent(student);
+    }
 }
