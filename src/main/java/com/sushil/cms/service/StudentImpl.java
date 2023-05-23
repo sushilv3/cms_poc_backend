02536ddd530ba8a415727c900c9700ef59976780
sushil <sushil.verma@walkingtree.tech>
2023-05-23 18:09:18 +0530

    added methods in studentImpl

--- src/main/java/com/sushil/cms/service/StudentImpl.java
+++ src/main/java/com/sushil/cms/service/StudentImpl.java
@@ -1,5 +1,17 @@
 package com.sushil.cms.service;
 
+import com.sushil.cms.entity.Student;
+
+import java.util.List;
+
 public interface StudentImpl {
+
+    public List<Student> getAll();
+
+    public Student findByStudentId(String id);
+
+    public Student updateStudentById(String id);
+
+    public Student deleteStudentById(String id);
 
 }
