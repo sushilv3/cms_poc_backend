62d552fa348d34174ef855364306f1a11db6a71c
sushil <sushil.verma@walkingtree.tech>
2023-05-24 18:51:55 +0530

    added findByCity method

--- src/main/java/com/sushil/cms/repository/StudentRepository.java
+++ src/main/java/com/sushil/cms/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import com.sushil.cms.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    public List<Student> findByCity(String city);
 }
